14dc884e5d6f15e15b1ad2a5aa2a7abf57e8a15b
Morumbi <zun0323@gmail.com>
2023-07-06 13:50:26 -0300

    make dummy DB

---
+++ src/lib/dummyList.ts
@@ -0,0 +1,56 @@
+export const dummyList = [
+  {
+    category: 'Work',
+    bgColor: 'FFA801',
+    list: [
+      {
+        id: 'w01',
+        contents: 'Call to Elon Musk',
+        star: false,
+        complete: false,
+      },
+      {
+        id: 'w02',
+        contents: 'Zoom with client',
+        star: false,
+        complete: false,
+      },
+      {
+        id: 'w03',
+        contents: 'Meeting with designers',
+        star: false,
+        complete: false,
+      },
+      {
+        id: 'w04',
+        contents: 'Lunch with Boss',
+        star: false,
+        complete: false,
+      },
+      {
+        id: 'w05',
+        contents: 'Submit expenses',
+        star: false,
+        complete: false,
+      },
+      {
+        id: 'w06',
+        contents: 'Book flight ticket',
+        star: false,
+        complete: false,
+      },
+      {
+        id: 'w07',
+        contents: 'Finish weekly report',
+        star: false,
+        complete: false,
+      },
+      {
+        id: 'w08',
+        contents: 'Clean my desk',
+        star: false,
+        complete: false,
+      },
+    ],
+  },
+];
