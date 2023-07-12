f11522fa6610bdf01972fcbe854be5fa78535a22
Morumbi <zun0323@gmail.com>
2023-07-12 13:20:58 -0300

    change dummyList / type

--- src/lib/type.ts
+++ src/lib/type.ts
@@ -1,13 +1,8 @@
 export interface IList {
   id: string;
-  category: string;
-  bgColor: string;
-  list: IItem[];
-}
-
-export interface IItem {
-  id: string;
   contents: string;
   star: boolean;
   complete: boolean;
+  category: string;
+  bgColor: string;
 }
